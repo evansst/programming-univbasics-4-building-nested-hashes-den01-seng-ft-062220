c5380298167d1670985011d620069fe6793b13e6
Sam Evans <evansst@icloud.com>
2020-06-16 01:34:44 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -3,7 +3,8 @@ new_hash = {railroads: {}}
 end
 
 def monopoly_with_second_tier
-  new_hash = base_hash[:railroads] = {
+  new_hash = base_hash
+  new_hash[:railroads] = {
     :pieces => 4,
     :rent_in_dollars => {},
     :names => {}
